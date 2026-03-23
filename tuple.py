83dd2e47c9f6996dbebb4500fe6d7a88f71520ba
rubypatel2197-stack <rubypatel2197@gmail.com>
2026-03-23 20:52:42 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,8 @@
+tup=(1,2,3,4,5)
+print("Original Tuple:",tup)
+
+# 1st operation: measuring the length of tuple
+print("Length of tuple:",len(tup))
+
+# 2nd operation: Slicing the tuple
+print("Elements from index 1 to 3:",tup[1:4])
